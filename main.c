7edae5c0333b4c9016762b2c3a5558a1dbecd00f
preshen28 <preshenc@aie.ac>
2025-09-05 22:50:27 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+
+    int a = 10;
+    int b = 3;
+
+//Question 1
+
+ printf("%d \n", a+b);
+ printf("%d \n", a-b);
+ printf("%d \n", a*b);
+ printf("%d \n", a/b);
+ printf("%d \n", a%b);
+
+
+}
